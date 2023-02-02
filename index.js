904d2223f075041839b6a6aa433c13a1e9af0ca8
SantiW01 <sweihmuller01@gmail.com>
2023-02-02 19:52:31 -0300

    feat: Add function for round played and return result

--- index.js
+++ index.js
@@ -12,3 +12,31 @@ function GetComputerChoice() {
 function GetPlayerChoice() {
   return String(prompt("What are you gonna choose?"));
 }
+
+function PlayRound(playerChoice, computerChoice) {
+  if (
+    playerChoice != "Rock" &&
+    playerChoice != "Scissor" &&
+    playerChoice != "Paper"
+  )
+    return "You must play with rock, paper or scissor";
+  if (
+    (playerChoice == "Paper" && computerChoice == "Paper") ||
+    (playerChoice == "Rock" && computerChoice == "Rock") ||
+    (playerChoice == "Scissor" && computerChoice == "Scissor")
+  ) {
+    return "It's a tie";
+  }
+
+  if (
+    (playerChoice == "Rock" && computerChoice == "Scissor") ||
+    (playerChoice == "Paper" && playerChoice == "Rock") ||
+    (playerChoice == "Scissor" && computerChoice == "Paper")
+  ) {
+    return "You win!";
+  } else {
+    return "You lose!";
+  }
+}
+
+console.log(PlayRound(GetPlayerChoice(), GetComputerChoice()));
