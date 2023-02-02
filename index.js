071bfe630b5850ce48a8b19d43e81cba556db049
SantiW01 <sweihmuller01@gmail.com>
2023-02-02 18:59:28 -0300

    feat: Add function for computer's play

---
+++ index.js
@@ -0,0 +1,10 @@
+function GetComputerChoice() {
+  switch (Math.floor(Math.random() * (3 - 1 + 1)) + 1) {
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissor";
+    case 3:
+      return "Rock";
+  }
+}
